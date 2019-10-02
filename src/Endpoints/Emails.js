cd19fa44631c364a356ee217fcb227a1a08c9fed
Gusztav Szikszai <guszti5@hotmail.com>
2019-10-02 09:04:39 +0200

    Update emails endpoint.
    - Added list function
    - Changed path from "email" to "emails"

--- src/Endpoints/Emails.js
+++ src/Endpoints/Emails.js
@@ -3,6 +3,16 @@ const Check = require('check-types');
 const Endpoint = require('../Endpoint');
 
 class Emails extends Endpoint {
+  list(page = 1, perPage = 10) {
+    Check.assert.number(page);
+    Check.assert.number(perPage);
+
+    return this.requestJSON('GET', 'emails', {
+      per_page: perPage,
+      page,
+    });
+  }
+
   send(subject, from, to, html, text) {
     Check.assert.maybe.string(html);
     Check.assert.maybe.string(text);
@@ -11,7 +21,7 @@ class Emails extends Endpoint {
     Check.assert.string(from);
     Check.assert.string(to);
 
-    return this.requestJSON('POST', 'email', {
+    return this.requestJSON('POST', 'emails', {
       subject,
       html,
       text,
--- test/Endpoints/Emails.test.js
+++ test/Endpoints/Emails.test.js
@@ -9,10 +9,30 @@ describe('Emails Endpoint', () => {
     from: 'test@user.com',
   };
 
+  describe('Listing emails', () => {
+    test('returns a list of emails', async () => {
+      nock('https://api.base-api.io')
+        .get('/v1/emails?per_page=10&page=1')
+        .reply(
+          200,
+          JSON.stringify({
+            items: [data],
+            metadata: { count: 1 },
+          }),
+        );
+
+      const response = await client.emails.list();
+
+      expect(response.metadata.count).toEqual(1);
+      expect(response.items.length).toEqual(1);
+      expect(response.items[0]).toEqual(data);
+    });
+  });
+
   describe('Sending an email', () => {
     test('it sends an email', async () => {
       nock('https://api.base-api.io')
-        .post('/v1/email')
+        .post('/v1/emails')
         .reply(200, JSON.stringify(data));
 
       const email = await client.emails.send(
